53657a31e8b8e520467012293cd98c50ef5cc998
Lula <loulazakaria02@gmail.com>
2022-12-14 14:49:05 +0300

    Update js

--- Development/code/phase-1/code-challenge-3/js/index.js
+++ Development/code/phase-1/code-challenge-3/js/index.js
@@ -1,19 +1,39 @@
-document.addEventListener("DOMContentLoaded", () => {
     //Fetch data
 function fetchData() {
     fetch('http://localhost:3000/films')
     .then(resp => resp.json())
-    .then(data => initialize(data))
+    .then(data => displayDetails(data))
 }
 
- function initialize(films){
+ function displayDetails(films){
     const main = document.querySelector('main')
     for(let film of films){
     const h1 = document.createElement('h1')
-        h1.innerHTML = film.name
+        h1.innerHTML = film.title
         main.appendChild(h1)
     }
 
+    function listenClick() {
+        const details = document.createElement('details')
+        details.innerHTML=`
+        <p>${film.id}</p>
+        <p>${film.runtime}</p>
+        <p>${film.capacity}</p>
+        <p>${film.showtime}</p>
+        <p>${film.tickets_sold}</p>
+        <p>${film.description}</p>
+        <img src>${film.image}
+        `
+        main.addEventListener('click', listenClick)
+    }
+
+
+    
  }
+
  
+ 
+ 
+document.addEventListener("DOMContentLoaded", () =>{
+    fetchData()
 })
