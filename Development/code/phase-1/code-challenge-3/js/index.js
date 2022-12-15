2f77bf619c60b36c275100efe357268b5d7af59f
Lula <loulazakaria02@gmail.com>
2022-12-15 10:43:37 +0300

    Restart the fetching process

--- Development/code/phase-1/code-challenge-3/js/index.js
+++ Development/code/phase-1/code-challenge-3/js/index.js
@@ -1,39 +1,13 @@
-    //Fetch data
-function fetchData() {
-    fetch('http://localhost:3000/films')
-    .then(resp => resp.json())
-    .then(data => displayDetails(data))
-}
+//Write the url 
+const url = "http://localhost:3000/films"
+//Create an eventlistener to load the content in the DOM first
+document.addEventListener('DOMContentLoaded', () => {
+    //Fetch the data
+    const fetchFirstFilm = () => {
+        fetch(url + id)
+       .then(response => response.json())
+       .then(data => { renderFirstFilm(data) })
+      }
+       fetchFirstFilm();
+  })
 
- function displayDetails(films){
-    const main = document.querySelector('main')
-    for(let film of films){
-    const h1 = document.createElement('h1')
-        h1.innerHTML = film.title
-        main.appendChild(h1)
-    }
-
-    function listenClick() {
-        const details = document.createElement('details')
-        details.innerHTML=`
-        <p>${film.id}</p>
-        <p>${film.runtime}</p>
-        <p>${film.capacity}</p>
-        <p>${film.showtime}</p>
-        <p>${film.tickets_sold}</p>
-        <p>${film.description}</p>
-        <img src>${film.image}
-        `
-        main.addEventListener('click', listenClick)
-    }
-
-
-    
- }
-
- 
- 
- 
-document.addEventListener("DOMContentLoaded", () =>{
-    fetchData()
-})
