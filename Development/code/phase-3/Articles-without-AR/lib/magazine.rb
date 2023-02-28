5155553ece1f6b41e23acabec66f2af49cd27305
Lula <loulazakaria02@gmail.com>
2023-02-28 05:10:01 +0300

    Added initialize

--- Development/code/phase-3/Articles-without-AR/lib/magazine.rb
+++ Development/code/phase-3/Articles-without-AR/lib/magazine.rb
@@ -3,4 +3,11 @@ class Magazine
     attr_reader :name, :category
 
     @@all = []
+
+    def initialize(name, category)
+        @name = name
+        @category = category
+        @articles = []
+        @@all << self
+    end
 end
