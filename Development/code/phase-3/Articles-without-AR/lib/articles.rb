5383370ac20296b8257fc8a14d74f5533c517201
Lula <loulazakaria02@gmail.com>
2023-02-28 05:19:33 +0300

    Added initialize for article

--- Development/code/phase-3/Articles-without-AR/lib/articles.rb
+++ Development/code/phase-3/Articles-without-AR/lib/articles.rb
@@ -1,12 +1,20 @@
 class Article
 
     attr_reader :name, :category
+    attr_reader :author, :magazine, :title
     @@all = []
 
     def initialize(name, category)
         @name = name
         @category = category
         @articles = articles
+        @@all << self
+    end
+
+    def initialize(author, magazine, title)
+        @author = author
+        @magazine = magazine
+        @title= title
         @@all << self
     end
 end
